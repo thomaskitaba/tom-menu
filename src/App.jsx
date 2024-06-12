1dd1f8061d6f2733217aa1fc84544843229ca493
thomaskitaba <thomas.kitaba@gmail.com>
2024-06-12 17:28:31 +0300

    added mega menu to tom-menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
 import { useState } from 'react';
+import MyContextProvider from './components/MyContextProvider';
 
 import './App.css';
 import Menu from './components/Menu';
 import Catagory from './components/Catagory';
 import Welcome from './components/Welcome';
-
+import Navigation from './components/MegaMenuNavbar';
+import MegaMenuNavbar from './components/MegaMenuNavbar'
 function App() {
-
 
   return (
     <>
-      <h1> Tom Menu </h1>
-      <Menu />
-      <Welcome />
-      <Catagory />
-      
+      <MyContextProvider>
+        <MegaMenuNavbar />
+        <Menu />
+        <Welcome />
+      </MyContextProvider>
     </>
   )
 }
