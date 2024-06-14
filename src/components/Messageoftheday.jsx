fc17e78636ba92cd0cf1f298c5906f0bb07bbe64
thomaskitaba <thomas.kitaba@gmail.com>
2024-06-14 23:11:03 +0300

    updated hover-forms position and meesageoftheday component

--- src/components/Messageoftheday.jsx
+++ src/components/Messageoftheday.jsx
@@ -1,14 +1,16 @@
 const Messageoftheday = () => {
   return (
     <>
+    <div className="message-container">
       <div className="message-image"> </div>
         <div className="message-text">
           <ul className="message-list">
-            <li>Today is Mothers Day </li>
+            <h1>Today is Mothers Day </h1>
             <li></li>
             <li></li>
           </ul>
         </div>
+    </div>
     </>
   )
 }
